b24efbfa77bfefb04735a71f2f98aa2fcd0fd9c4
Joshua Conero <conero@163.com>
2025-04-13 18:35:53 +0800

    feat: 实现命令解析

--- src/cli/arg.zig
+++ src/cli/arg.zig
@@ -17,11 +17,37 @@ pub const Arg = struct {
 
     /// 指定参数列表来解析命令行
     pub fn args(argsList: [][:0]u8) *const Arg {
+        var command: []u8 = "";
         for (argsList, 0..) |arg, index| {
             std.debug.print("{d} -> {s}\n", .{ index, arg });
+            const dOpt = detectOption(arg, true);
+            if (dOpt.@"1") {
+                continue;
+            }
+            if (command.len == 0) {
+                command = arg;
+            }
         }
         return &Arg{
-            //.options = &[_][:0]u8{0},
+            .command = command,
         };
     }
 };
+
+/// 检测变量是否为选项
+pub fn detectOption(vString: []u8, supportLong: bool) struct { []u8, bool } {
+    const vLen = vString.len;
+    if (vLen == 0) {
+        return .{ "", false };
+    }
+
+    // 长选项
+    if (vLen > 1 and supportLong and std.mem.eql(u8, vString[0..2], "--")) {
+        return .{ vString[2..], true };
+    }
+
+    if (vString[0] == '-') {
+        return .{ vString[1..], true };
+    }
+    return .{ "", false };
+}
