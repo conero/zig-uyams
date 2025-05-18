760ad0530633fefc9d01c9e222749852a6e092e8
Joshua Conero <conero@163.com>
2025-05-19 00:03:19 +0800

    pref(muber): 初步实现字符串替换

--- src/number.zig
+++ src/number.zig
@@ -5,18 +5,17 @@ const std = @import("std");
 /// 支持 123.10K（K/千，W/万，M/百万，B/亿，T/Tensor） 等英文标识
 ///
 /// 支持 123.10万（百、千、万、亿） 等英文标识
-pub fn strToInt(vNumber: []const u8) i128 {
+pub fn strToInt(alloc: std.mem.Allocator, vNumber: []const u8) i128 {
     // 操作代码（实际编译错误）： https://github.com/capy-ui/capy/blob/4d41d962e6d0404a7beb9c37c1a5ba68556f9efb/src/assets.zig#L51
-    const rplStr: [4096]u8 = undefined;
     const rplSize = std.mem.replacementSize(u8, vNumber, ",", "");
-    //_ = std.mem.replace(u8, vNumber, "_", "", rplStr);
-    _ = std.mem.replace(u8, vNumber, ",", "", &rplStr);
+    const rplStr = alloc.alloc(u8, rplSize) catch unreachable;
+    _ = std.mem.replace(u8, vNumber, ",", "", rplStr);
     //_ = std.mem.replace(u8, rplStr, ",", "", rplStr);
-    std.debug.print("strToInt: {s}\n", .{rplStr[0..rplSize]});
+    std.debug.print("strToInt: {s}\n", .{rplStr});
     return 0;
 }
 
 test "strToInt base test" {
-    strToInt("100_000");
+    strToInt(std.testing.allocator, "100_000");
     try std.testing.expectEqual(strToInt("100_000"), 100000);
 }
