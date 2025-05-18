b9af50bd06eba38ae2a1224fc7ecc59cf707bf13
Joshua Conero <conero@163.com>
2025-05-18 23:50:41 +0800

    feat(!stuck): 初步加入 strToInt 用于数字转字符串但编译错误

--- src/lib.zig
+++ src/lib.zig
@@ -5,15 +5,16 @@ const std = @import("std");
 const variable = @import("variable.zig");
 const testing = std.testing;
 
-// 命令行解析
+/// 命令行解析
 pub const cli = @import("cli/cli.zig");
-// 版本号
+/// 版本号
 pub const Version = variable.Version;
-// 发布日期
+/// 发布日期
 pub const Release = variable.Release;
-// 工具辅助处理库
+/// 工具辅助处理库
 pub const util = @import("util.zig");
-
+/// 数字处理库
+pub const number = @import("number.zig");
 // 原示例代码
 pub fn add(a: i32, b: i32) i32 {
     return a + b;
---
+++ src/number.zig
@@ -0,0 +1,22 @@
+const std = @import("std");
+
+/// 字符串转数字，支持如"100_000"、"100,000"这种带下划线的数字
+///
+/// 支持 123.10K（K/千，W/万，M/百万，B/亿，T/Tensor） 等英文标识
+///
+/// 支持 123.10万（百、千、万、亿） 等英文标识
+pub fn strToInt(vNumber: []const u8) i128 {
+    // 操作代码（实际编译错误）： https://github.com/capy-ui/capy/blob/4d41d962e6d0404a7beb9c37c1a5ba68556f9efb/src/assets.zig#L51
+    const rplStr: [4096]u8 = undefined;
+    const rplSize = std.mem.replacementSize(u8, vNumber, ",", "");
+    //_ = std.mem.replace(u8, vNumber, "_", "", rplStr);
+    _ = std.mem.replace(u8, vNumber, ",", "", &rplStr);
+    //_ = std.mem.replace(u8, rplStr, ",", "", rplStr);
+    std.debug.print("strToInt: {s}\n", .{rplStr[0..rplSize]});
+    return 0;
+}
+
+test "strToInt base test" {
+    strToInt("100_000");
+    try std.testing.expectEqual(strToInt("100_000"), 100000);
+}
